7db8d58ed767296e724807842a723c8af845134c
kattn <mgbjerke@gmail.com>
2016-10-20 14:40:19 +0200

    La til footer

--- Fabo/javascript/foothead.js
+++ Fabo/javascript/foothead.js
@@ -21,3 +21,15 @@ document.getElementsByTagName("HEADER")[0].innerHTML =
 		"<li><a href=\"lenker.html\">Lenker</a></li>" +
 		"<li><a href=\"nytt.html\">Nytt fra studien</a></li>" +
 	"</ul>";
+
+document.getElementsByTagName("FOOTER")[0].innerHTML = 
+	"<a href=\"media.html\">Studien i media</a><br>" +
+	"<a href=\"publikasjoner.html\">Publikasjoner</a><br>" +
+	"<a href=\"sponsorer.html\">Sponsorer</a>" + 
+	"<div id=\"kontakt\">" +
+	"<p>Kontakt:</p>" +
+	"<p>petur.juliusson@k2.uib.no</p>" +
+	"<p>91767373</p>" +
+	"</div>" +
+	"<img src=\"img/helsebergen_logo.png\" alt=\"logo helsebergen\">" +
+	"<img src=\"img/uib_logo.png\" alt=\"logo uib\">";
