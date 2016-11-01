c03f06655a01923c8bf1a267d16a8ff778485a7c
Renate Bech-Sørensen <Renate Bech-Srensen>
2016-11-01 18:13:52 +0100

    NYTT DESIGN. Bruker et random bilde fra google for å illustrere hvordan det kan se ut.

--- Fabo/javascript/foothead.js
+++ Fabo/javascript/foothead.js
@@ -1,7 +1,4 @@
 document.getElementsByTagName("HEADER")[0].innerHTML = 
-	"<a href=\"index.html\">" +
-		"<img src=\"img/headerbilde.png\" alt=\"header\">" +
-	"</a>" + 
 	"<ul class=\"navbar\">" +
 		"<li class=\"dropdown\">" +
 			"<a href=\"#\" class=\"dropbtn\">FABO-studien</a>" +
@@ -14,13 +11,16 @@ document.getElementsByTagName("HEADER")[0].innerHTML =
 		"<li class=\"dropdown\">" +
 			"<a href=\"#\" class=\"dropbtn\">Materiell</a>" +
 			"<div class=\"dropdowncnt\">" +
-				"<a href=\"helse.html\">For hestesøstre</a>" +
+				"<a href=\"helse.html\">For helsesøstre</a>" +
 				"<a href=\"familie.html\">For familie</a>" +
 			"</div>" +
 		"</li>" +	
 		"<li><a href=\"lenker.html\">Lenker</a></li>" +
 		"<li><a href=\"nytt.html\">Nytt fra studien</a></li>" +
-	"</ul>";
+	"</ul>" +
+	//TEST
+	"<img src=\"img/random.jpg\" alt=\"header\">" +
+	"</a>";
 
 document.getElementsByTagName("FOOTER")[0].innerHTML = 
 	"<table class=\"footerTable\"> " +
