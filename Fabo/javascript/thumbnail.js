77e52791433fbb257b27e3e297c7f107d87a9137
Oddrun Petursdottir <oddrun.petursdottir@gmail.com>
2016-11-03 13:40:58 +0100

    begynt på js thumbnail og lagt til link i media

---
+++ Fabo/javascript/thumbnail.js
@@ -0,0 +1,13 @@
+
+var img1 = document.getElementById("tonje");
+
+img1.addEventListener('mouseover', function (event) {
+	img1.style.height="200px";
+	img1.style.width="300px";
+});
+
+img1.addEventListener('mouseout', function (event){
+	img1.style.height="120px";
+	img1.style.width="190px";
+
+});
