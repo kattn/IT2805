2aa5de80c74b0eb2f185cf7dd4fe7724b104227e
kattn <mgbjerke@gmail.com>
2016-11-13 19:41:56 +0100

    Jobbes med

--- Fabo/javascript/search.js
+++ Fabo/javascript/search.js
@@ -6,6 +6,11 @@ const pages = [
 	"sponsorer.html" ];
 
 var xhr = [];
+var txt = [];
+
+/*var iframe = document.createElement("IFRAME");
+iframe.setAttribute("display","none");
+document.getElementsByTagName("BODY")[0].appendChild(iframe);*/
 
 /* Search collect all the text of all pages into txt[] */
 /* Not very effective since all text is stored in memory, but do be done in time */
@@ -28,26 +33,28 @@ function(event){
 	event.preventDefault();
 	console.log("Submit clicked.");
 
+	var finds = [];
+
 	for(var i = 0, len = pages.length; i < len; i++){
-		searchPage(xhr[i].responsetext,document.getElementById("txt").value);
+		finds.push(searchPage(xhr[i].responseText,document.getElementById("txt").value));
 	}
+
 	
 });
 
 
 /* Search a page for the giveN search term and returns all placements for it*/
-function searchPage(doc, searchTerm){
+function searchPage(cont, searchTerm){
 
 	console.log("hello")
 
-	var cont = doc.getElementById("content").textContent;
 	var placements = [];
 	var placement = cont.search(searchTerm);
 
 	/* Find all the placments of the search term and add them to the array */
 	while(placement != -1){
 		placements.push(placement);
-		cont = cont.substr(placement);
+		cont = cont.substr(placement)[1];
 		placement = cont.search(searchTerm);
 	}
 
