99c1fa6f0c5ea88873cfc63d575ec611e7fcf160
kattn <mgbjerke@gmail.com>
2016-11-13 20:09:48 +0100

    Kommentert ferdig

--- Fabo/javascript/scroll.js
+++ Fabo/javascript/scroll.js
@@ -9,28 +9,45 @@ function currentYPosition() {
     return 0;
 }
 
+/* returns a y position of en element based on the ID */
 function elmYPosition(eID) {
     var elm = document.getElementById(eID);
     var y = elm.offsetTop;
     var node = elm;
+
+    /* Adds the elements offset, and the parentNodes offset, to top to y untill
+    all the parents, except the body tag, is accounted for */
     while (node.offsetParent && node.offsetParent != document.body) {
         node = node.offsetParent;
         y += node.offsetTop;
     } return y;
 }
 
+/* Scroll smoothly from the current possition to the target eID */
 function smoothScroll(eID) {
     var startY = currentYPosition();
     var stopY = elmYPosition(eID);
+    
+    /* Guarentees the distance is positive */
     var distance = stopY > startY ? stopY - startY : startY - stopY;
+
+    /* No need to smoothscroll over short distances */
     if (distance < 100) {
         scrollTo(0, stopY); return;
     }
+
+    /* Sets the speed based on the distance, maxing at 20 */
     var speed = Math.round(distance / 100);
     if (speed >= 20) speed = 20;
+
+    /* Sets a step size */
     var step = Math.round(distance / 25);
+
+    /* Makes sure the scroll direction is right */
     var leapY = stopY > startY ? startY + step : startY - step;
     var timer = 0;
+
+    /* Scrolling, one loop for each direction */
     if (stopY > startY) {
         for ( var i=startY; i<stopY; i+=step ) {
             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
