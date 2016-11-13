38ba00136d0bb052501ef3d94197b7978629d76b
kattn <mgbjerke@gmail.com>
2016-11-13 20:13:14 +0100

    Kommentert ferdig

--- Fabo/javascript/thumbnail.js
+++ Fabo/javascript/thumbnail.js
@@ -1,6 +1,8 @@
 
 var images = document.getElementsByClassName("bilde_media1");
 
+/* For alle bildene, hvis de hovers over sett nytt classe navn som har annen
+css. CSSen til bilde_media2 uthever bilde. */
 for (var i = 0, len = images.length; i < len; i++){
 	images[i].addEventListener('mouseover', function (event) {
 		this.setAttribute("class", "bilde_media2");
