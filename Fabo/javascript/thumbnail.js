828e96fa60062cde4666c206147cdc4d99a780b7
kattn <mgbjerke@gmail.com>
2016-11-13 20:10:37 +0100

    Fjerna console.log

--- Fabo/javascript/thumbnail.js
+++ Fabo/javascript/thumbnail.js
@@ -1,10 +1,7 @@
 
 var images = document.getElementsByClassName("bilde_media1");
 
-console.log(images)
-
 for (var i = 0, len = images.length; i < len; i++){
-	console.log(images[i]);
 	images[i].addEventListener('mouseover', function (event) {
 		this.setAttribute("class", "bilde_media2");
 	});
