a061a563f033b17e4b0831b9462ec56e85628ded
kattn <mgbjerke@gmail.com>
2016-11-13 20:31:48 +0100

    Kommentert ferdig

--- Fabo/javascript/responsive.js
+++ Fabo/javascript/responsive.js
@@ -1,5 +1,7 @@
 
 /*Kode inspirert og hentet fra http://www.w3schools.com/howto/howto_js_topnav.asp*/
+
+/* Legger til responsive i klasse navnet for css for mobil/nettbrett */
 function responsiveFunction(){
 	var x = document.getElementById("myNavbar");
 	if (x.className === "navbar") {
@@ -10,6 +12,8 @@ function responsiveFunction(){
 	}
 }
 
+/* Legger til responsive i klasse navnet for css for mobil/nettbrett.
+Her også basert på hvilke under meny som åpnes */
 function dropdownFunction(isMateriell){
 	var x;
 	if(isMateriell){
